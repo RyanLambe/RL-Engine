5b82d31192224f3b38e3ec7ab58f95d864a1370e
Ryan Lambe <ryanlambe@hotmail.com>
2024-07-04 22:53:09 -0400

    open windows by default

--- Editor/src/main.cpp
+++ Editor/src/main.cpp
@@ -1,6 +1,8 @@
 #include "Editor.h"
 #include "ToolBar.h"
 #include "project/ProjectManager.h"
+#include "windows/Console.h"
+#include "windows/SystemsManager.h"
 
 using namespace rl::ed;
 
@@ -13,6 +15,10 @@ int main()
 
     toolbar = std::make_shared<ToolBar>();
     Editor::AddGuiElement(toolbar);
+
+    // open default windows
+    Console::OpenWindow();
+    SystemsManager::OpenWindow();
 
     // main loop
     while (Editor::Update())
